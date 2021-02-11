edf056e563d65bddd20fef6f3af8de98ead05cec
yicheng <yicheng.881218@google.com>
2021-02-12 00:51:04 +0800

    Project init

--- app.py
+++ app.py
@@ -285,4 +285,4 @@ def edit_product_page(pid):
 if __name__ == '__main__':
     port = int(os.environ.get("PORT", 5000))
     # 應用程式開始運行
-    app.run(host='0,0,0,0', port=port)
+    app.run(debug=True, host='0,0,0,0', port=port)
